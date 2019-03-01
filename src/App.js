9838bc80469a7b9c086a572558fb58c661bd4753
keoghpe@tcd.ie <keoghpe@tcd.ie>
2019-03-01 21:52:40 +0000

    Play audio from oscillator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,7 +15,24 @@ const defaultNotes = [
   [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
   [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
   [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
 ];
+
+const scale = [
+  261.63,
+  246.94,
+  233.08,
+  220.00,
+  207.65,
+  196.00,
+  185.00,
+  174.61,
+  164.81,
+  155.56,
+  164.83,
+  138.59,
+  130.81,
+]
 
 const App = () => {
 
@@ -23,15 +40,23 @@ const App = () => {
   const [column, setColumn] = useState(0);
 
   useEffect(() => {
-    // var ctx = new AudioContext();
-    // var o = ctx.createOscillator();
-    // o.frequency.value = 261.63;
-    // o.start(0);
-    // o.connect(ctx.destination);
+    const tickMs = 80;
 
     let interval = setInterval(() => {
       setColumn((column + 1) % 16)
-    }, 80)
+    }, tickMs)
+
+    var ctx = new AudioContext();
+
+    notes.forEach((row, index) => {
+      if(notes[index][column]) {
+        var o = ctx.createOscillator();
+        o.frequency.value = scale[column];
+        o.start(0);
+        o.stop(tickMs / 1000);
+        o.connect(ctx.destination);
+      }
+    })
 
     return () => {
       clearInterval(interval)
@@ -62,7 +87,8 @@ const NoteRow = ({index, noteRow, toggleCell, column}) => {
   return (
     <div className="NodeRow">
       {
-        noteRow.map((active, jndex) => <Note index={index} jndex={jndex} active={active} column={column} toggleCell={toggleCell} />)
+        noteRow.map((active, jndex) => <Note index={index} jndex={jndex} active={active} column={column}
+                                             toggleCell={toggleCell}/>)
       }
     </div>
   )
@@ -72,7 +98,9 @@ const Note = ({active, toggleCell, index, jndex, column}) => {
   let activeClass = active ? 'active' : '';
   let currentClass = jndex === column ? 'current' : '';
 
-  return (<div className={`Note ${activeClass} ${currentClass}`} onClick={() => {toggleCell(index, jndex)}}></div>)
+  return (<div className={`Note ${activeClass} ${currentClass}`} onClick={() => {
+    toggleCell(index, jndex)
+  }}></div>)
 }
 
 
